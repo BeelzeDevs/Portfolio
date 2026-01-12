cdc71458748d64e2f8f7389e90f3f67c9e1479d3
Ignacio Aprile <ignacioaprile@hotmail.com>
2026-01-11 22:46:46 -0300

    agregado warning sobre el cold-start de los proyectos, en la page projects

--- portfolio/app/(routes)/projects/page.tsx
+++ portfolio/app/(routes)/projects/page.tsx
@@ -3,6 +3,7 @@ import CircleImage from "@/components/circle-image";
 import ContainerPage from "@/components/container";
 import Projects from "@/components/projects";
 import TransitionPage from "@/components/transition-page";
+import { IoWarningOutline } from "react-icons/io5";
 
 const ProjectsPage = () => {
     return(
@@ -12,6 +13,18 @@ const ProjectsPage = () => {
                 <AvatarProjecto />
                 <CircleImage />
                 
+                <section className="relative w-full p-10 my-10 flex items-center justify-center cursor-default">
+                    <div role="alert"
+                        className="absolute h-full w-full max-w-5xl flex justify-center items-center ring-2 ring-amber-500/70 hover:ring-amber-400 transition rounded-xl p-4 gap-4"
+                    >
+                        <IoWarningOutline className="w-10 h-10 min-w-8 min-h-8 text-amber-500 hover:text-amber-400" />
+                        <p className="font-semibold text-white text-base md:text-lg leading-tight">
+                        Los deploys pueden tardar hasta <span className="text-amber-500 hover:text-amber-400">50 seg</span> hasta iniciar tanto frontend como backend <span className="text-amber-500 hover:text-amber-400 ">(cold-start)</span>.                            
+                        Gracias por tu paciencia.
+                        </p>
+                    </div>
+                </section>
+
                 <Projects />
             </ContainerPage>
         </main>
